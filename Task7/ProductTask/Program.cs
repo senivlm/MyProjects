3fe7b898f620e01dd9ad8fa10c543e58b3aeabad
Oleksandr <fly96668@gmail.com>
2022-06-17 15:35:10 +0300

    some fixes

--- Task7/ProductTask/Program.cs
+++ Task7/ProductTask/Program.cs
@@ -11,10 +11,10 @@ class Program
 			Storage storage = new Storage();
 			LogHandler<Product> logHandler = new LogHandler<Product>(@"D:\allprjcts\MyProjects\Task7\ProductTask\RegistLogs.txt");
 			storage.AddItemsFromFile(@"D:\allprjcts\MyProjects\Task7\ProductTask\ProductList.txt", logHandler);
-            Console.WriteLine(storage[1]); // object before change
-			logHandler.GetLogObject(1).ChangePrice(70); // to change object from logs
-            Console.WriteLine(storage[1]); // object after change
-			
+            Console.WriteLine(storage[1]); // об*єкт до зміни
+			logHandler.GetLogObject(1).ChangePrice(70); // зміна об*єкту з обробника логів по відповідному індексу
+            Console.WriteLine(storage[1]); // об*єкт після
+
 		}
 		catch(FileNotFoundException ex)
         {
