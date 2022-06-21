54f8327f53b84c629f125542517ed787db570561
Olexandr <fly96668@gmail.com>
2022-06-22 01:00:22 +0300

    fixes

--- Task9/Task9/Program.cs
+++ Task9/Task9/Program.cs
@@ -12,8 +12,20 @@
 
             CurrencyExchange.ReadExchangeRateFromFile(@"D:\MyProjects\Task9\Task9\Course.txt");
 
+            Console.WriteLine("Введіть валюту ");
+            string currency = Console.ReadLine();
+            object res;
+            if(Enum.TryParse(typeof(Currencies), currency, out res))
+            {
+                PriceKurantHandler.PrintPriceKurant(menu, priceKurant, (Currencies)res, @"D:\MyProjects\Task9\Task9\result.txt");
+            }
+            else
+            {
+                return;
+            }
+
             //Вивід для різних валют:
-            PriceKurantHandler.PrintPriceKurant(menu, priceKurant, Currencies.USD, @"D:\MyProjects\Task9\Task9\result.txt");
+            //PriceKurantHandler.PrintPriceKurant(menu, priceKurant, Currencies.USD, @"D:\MyProjects\Task9\Task9\result.txt");
             PriceKurantHandler.PrintPriceKurant(menu, priceKurant, Currencies.UAH, @"D:\MyProjects\Task9\Task9\result.txt");
             PriceKurantHandler.PrintPriceKurant(menu, priceKurant, Currencies.EURO, @"D:\MyProjects\Task9\Task9\result.txt");
         }
