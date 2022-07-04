5d95e486d02cf40b2408d21dd436ae28a391dea6
Olexandr <fly96668@gmail.com>
2022-07-04 15:37:49 +0300

    fixes

--- Task 12/task 12 part 3 (польский калькулятор)/task 12 part 3 (польский калькулятор)/Program.cs
+++ Task 12/task 12 part 3 (польский калькулятор)/task 12 part 3 (польский калькулятор)/Program.cs
@@ -7,6 +7,9 @@ class Program
     {
         //сам ввід не прив*язаний до логіки, по суті, ми можемо його змінити на читання з файлу
         string x = "";
+        //додавання операцій
+        Calculator calculator = new Calculator();
+        calculator.AddBinaryOperator("%", (x, y) => x % y);
         var calc = new PolishCalculatorService("");
         do
         {
@@ -16,14 +19,12 @@ class Program
             {
                 calc.ChangeLine(x);
                 //compute це дженерік метод, щоб можливо було вибирати реалізації калькулятора, які реалізують
-                //інтерфейс icalculator
-                Console.WriteLine("Result: " + calc.Compute<Calculator>());
+                //інтерфейс icalculator. Є ще також перегрузка для Сompute, якій можна передавати
+                //екземпляри калькулятора, які містять наприклад нові операції
+                Console.WriteLine("Result: " + calc.Compute(calculator));
             }
         }
         while (x != "exit" && x != "EXIT");
-            //додавання операцій
-        Calculator calculator = new Calculator();
-        calculator.AddBinaryOperator("%", (x, y) => x % y);
 
     }
 }
