0dfb6b1fdc38b07063cbdc6813596db04234b300
Olexandr <fly96668@gmail.com>
2022-07-04 15:40:53 +0300

    fixes

--- Task 12/task 12 part 3 (польский калькулятор)/task 12 part 3 (польский калькулятор)/Program.cs
+++ Task 12/task 12 part 3 (польский калькулятор)/task 12 part 3 (польский калькулятор)/Program.cs
@@ -5,26 +5,32 @@ class Program
 {
     public static void Main()
     {
-        //сам ввід не прив*язаний до логіки, по суті, ми можемо його змінити на читання з файлу
-        string x = "";
-        //додавання операцій
-        Calculator calculator = new Calculator();
-        calculator.AddBinaryOperator("%", (x, y) => x % y);
-        var calc = new PolishCalculatorService("");
-        do
+        try
         {
-            Console.WriteLine("Enter example");
-            x = Console.ReadLine();
-            if (x != "exit" && x != "EXIT")
+            //сам ввід не прив*язаний до логіки, по суті, ми можемо його змінити на читання з файлу
+            string x = "";
+            //додавання операцій
+            Calculator calculator = new Calculator();
+            calculator.AddBinaryOperator("%", (x, y) => x % y);
+            var calc = new PolishCalculatorService("");
+            do
             {
-                calc.ChangeLine(x);
-                //compute це дженерік метод, щоб можливо було вибирати реалізації калькулятора, які реалізують
-                //інтерфейс icalculator. Є ще також перегрузка для Сompute, якій можна передавати
-                //екземпляри калькулятора, які містять наприклад нові операції
-                Console.WriteLine("Result: " + calc.Compute(calculator));
+                Console.WriteLine("Enter example");
+                x = Console.ReadLine();
+                if (x != "exit" && x != "EXIT")
+                {
+                    calc.ChangeLine(x);
+                    //compute це дженерік метод, щоб можливо було вибирати реалізації калькулятора, які реалізують
+                    //інтерфейс icalculator. Є ще також перегрузка для Сompute, якій можна передавати
+                    //екземпляри калькулятора, які містять наприклад нові операції
+                    Console.WriteLine("Result: " + calc.Compute(calculator));
+                }
             }
+            while (x != "exit" && x != "EXIT");
         }
-        while (x != "exit" && x != "EXIT");
-
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
     }
 }
