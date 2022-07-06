a6bfc90da45a77c5a99dd6cbee47a02d66160597
senivlm <91743906+senivlm@users.noreply.github.com>
2022-07-06 21:46:44 +0300

    Update IStorage.cs

--- Task 11/Task 11 part 1/Task 11/Storage/IStorage.cs
+++ Task 11/Task 11 part 1/Task 11/Storage/IStorage.cs
@@ -1,7 +1,7 @@
 ﻿using Task_11.Products;
 
 namespace Task_11.Storage
-{
+{// Краще розділити на 2 інтерфейси згідно Solid
     public interface IStorage<T> : IEnumerable<T> where T : Product
     {
         public T this[int i] { get; }
