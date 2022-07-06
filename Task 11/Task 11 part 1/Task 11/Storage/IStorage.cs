c26611cb86929f7691d13da37cd07f4ff23e49f5
senivlm <91743906+senivlm@users.noreply.github.com>
2022-07-06 21:48:35 +0300

    Update IStorage.cs

--- Task 11/Task 11 part 1/Task 11/Storage/IStorage.cs
+++ Task 11/Task 11 part 1/Task 11/Storage/IStorage.cs
@@ -1,7 +1,7 @@
 ﻿using Task_11.Products;
 
 namespace Task_11.Storage
-{
+{// А чи не потрібно для абстракції використати IProduct?
     public interface IStorage<T> : IEnumerable<T> where T : Product
     {
         public T this[int i] { get; }
