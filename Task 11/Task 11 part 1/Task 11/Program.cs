9675ba0c4f7e0ab3b39f4c52125ae97441178b78
senivlm <91743906+senivlm@users.noreply.github.com>
2022-07-06 21:43:41 +0300

    Update Program.cs

--- Task 11/Task 11 part 1/Task 11/Program.cs
+++ Task 11/Task 11 part 1/Task 11/Program.cs
@@ -7,6 +7,7 @@ class Program
 	static void Main(string[] args)
 	{
 		var storage = new Storage<Product>();
+		// Вдале використання лямбди
 		storage.ProductAvaliableEvent += a => Console.WriteLine($"({a}) is available!");
 		SomeProduct someProduct = new SomeProduct("apple", 10, 10);
 		SomeProduct someProduct1 = new SomeProduct("apple", 15, 10);
