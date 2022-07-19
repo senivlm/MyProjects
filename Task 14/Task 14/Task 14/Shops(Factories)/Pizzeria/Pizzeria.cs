e6621f563f163faab12dae28c1999752984976e4
Olexandr <fly96668@gmail.com>
2022-07-19 11:41:01 +0300

    fixes

--- Task 14/Task 14/Task 14/Shops(Factories)/Pizzeria/Pizzeria.cs
+++ Task 14/Task 14/Task 14/Shops(Factories)/Pizzeria/Pizzeria.cs
@@ -3,7 +3,8 @@ using Task_14.Products.Interfaces;
 
 namespace Task_14.Shops_Fabrics_.Pizzeria
 {
-    //Абстрактна фабрика
+    //Реалізація піцерії більше схожа на фабричний метод,
+    //але легко розширити її до абстрактної фабрики.
     public abstract class Pizzeria
     {
         public abstract IPizza GetPizza(PizzaTypes pizzaType);
